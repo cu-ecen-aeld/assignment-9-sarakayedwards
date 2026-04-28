6c246c071bfd52409e0e03bed7f53789020a2b5c
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2026-04-27 22:41:43 -0700

    for assignment 8

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,13 +5,15 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = 2a0864f91a7c8fd22a0843ae6c20395e0030289b
+AESD_ASSIGNMENTS_VERSION = f9a59b313a93085cebf02d33f397a0e051b77575
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-sarakayedwards.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+
+AESD_ASSIGNMENTS_CONF_OPTS += -DUSE_AESD_CHAR_DEVICE=1
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
---
+++ base_external/package/aesdchar/aesdchar.mk
@@ -0,0 +1,27 @@
+
+##############################################################
+#
+# AESDCHAR
+#
+##############################################################
+
+AESDCHAR_VERSION = f9a59b313a93085cebf02d33f397a0e051b77575
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+AESDCHAR_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-sarakayedwards.git
+AESDCHAR_SITE_METHOD = git
+AESDCHAR_GIT_SUBMODULES = YES
+
+AESDCHAR_MODULE_SUBDIRS = aesd-char-driver
+
+AESDCHAR_CONF_OPTS += -DUSE_AESD_CHAR_DEVICE=1
+
+define AESDCHAR_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8-buildroot/drivertest.sh $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
